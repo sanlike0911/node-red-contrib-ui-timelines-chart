05863cd9a9ee7d43235dac111091218480828f99
hisashi <sanlike0911@gmail.com>
2021-08-30 11:14:53 +0900

    chg: enable functionExternalModules

--- data/settings.js
+++ data/settings.js
@@ -256,7 +256,7 @@ module.exports = {
     },
 
     // Allow the Function node to load additional npm modules
-    functionExternalModules: false,
+    functionExternalModules: true,
 
     // `global.keys()` returns a list of all properties set in global context.
     // This allows them to be displayed in the Context Sidebar within the editor.
